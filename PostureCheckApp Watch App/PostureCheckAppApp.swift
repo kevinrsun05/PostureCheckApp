53e88c7984fb547096eb0fcf14f65b9f4f053eb5
Kevin Sun <krsun05@gmail.com>
2024-08-15 09:21:20 -0400

    Initial Commit

---
+++ PostureCheckApp Watch App/PostureCheckAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostureCheckAppApp.swift
+//  PostureCheckApp Watch App
+//
+//  Created by Kevin Sun on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PostureCheckApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
