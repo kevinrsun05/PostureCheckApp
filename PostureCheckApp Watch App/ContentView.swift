5d77cf3a7ccebd4a77e8cc683442caf0c0e5a6f9
Kevin Sun <krsun05@gmail.com>
2024-08-15 11:11:32 -0400

    Basic UI Done

--- PostureCheckApp Watch App/ContentView.swift
+++ PostureCheckApp Watch App/ContentView.swift
@@ -1,21 +1,39 @@
-//
-//  ContentView.swift
-//  PostureCheckApp Watch App
-//
-//  Created by Kevin Sun on 8/15/24.
-//
-
 import SwiftUI
+import CoreMotion
+import UserNotifications
 
 struct ContentView: View {
+    @State private var selectedOption = "30 min"
+    @State private var selectedTimeInterval: TimeInterval = 1800
+    let options = ["30 min", "1 hr", "2 hr"]
+    let timeIntervals: [TimeInterval] = [1800, 3600, 7200]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("Reminders every:")
+                    .font(.system(size: 16))
+                    .padding(.bottom, 5)
+                
+                Picker("Select Interval", selection: $selectedOption) {
+                    ForEach(options, id: \.self) { option in
+                        Text(option)
+                            .font(.system(size: 16))
+                    }
+                }
+                .labelsHidden()
+                .pickerStyle(.wheel)
+                .onChange(of: selectedOption) {
+                    if let index = options.firstIndex(of: selectedOption) {
+                        selectedTimeInterval = timeIntervals[index]
+                    }
+                }
+            }
+            .padding()
+            .frame(height: 90)
+            .navigationTitle("Posture")
+            Spacer()
         }
-        .padding()
     }
 }
 
